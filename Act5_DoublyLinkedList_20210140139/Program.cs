703d8ea448beec1316c5992b6b3345701ae19be6
sarahannata <sarahannata@gmail.com>
2022-11-16 16:26:54 +0700

    Membuat function public

--- Act5_DoublyLinkedList_20210140139/Program.cs
+++ Act5_DoublyLinkedList_20210140139/Program.cs
@@ -4,6 +4,13 @@ namespace DoublyLinkedList
 {
     class Node
     {
-
+        /*Node class represents the node of doubly linked list.
+         * It consist of the information part and links to
+         * its succeeding and preceeding nodes
+         * in terms of next and previous nodes.*/
+        public int rollNumber;
+        public string name;
+        public Node next;/*points to the succeeding node*/
+        public Node prev;/*points to the preceeding node*/
     }
 }

eab06b759675b645c59ad624dc4c58d31243bd52
sarahannata <sarahannata@gmail.com>
2022-11-16 16:45:03 +0700

    Membuat if

--- Act5_DoublyLinkedList_20210140139/Program.cs
+++ Act5_DoublyLinkedList_20210140139/Program.cs
@@ -31,7 +31,21 @@ namespace DoublyLinkedList
             Node newnode = new Node();
             newnode.rollNumber = rollNo;
             newnode.name = nm;
-
+            /*Checks if the list is empty*/
+            if (START == null || rollNo == START.rollNumber)
+            {
+                if ((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+                newnode.next = START;
+                if (START != null)
+                    START.prev = newnode;
+                newnode.prev = null;
+                START = newnode;
+                return;
+            }
         }
     }
 }
